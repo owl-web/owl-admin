b5012ba05f2256116c50a3eb06c4f25c77baf9de
Circleee <upupwangxianxi@163.com>
2022-06-13 23:11:57 +0800

    feat: 🦉 localstorge 封装 (#4)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import app from './App.vue'
 import ElementPlus from 'element-plus'
 import "element-plus/dist/index.css"
 import router from './router/index'
-
-createApp(App).use(router).use(ElementPlus).mount('#app')
+import storge from './utils/storge'
 
 console.log(import.meta.env)
+
+const App = createApp(app)
+
+App.config.globalProperties.$http = http
+App.config.globalProperties.$storge = storge
+
+
+App.use(router).use(ElementPlus).mount('#app')
+
+
+
