1017a00ba52f25a8ef6c23e49767733de5804fc6
Circleee <upupwangxianxi@163.com>
2022-07-07 18:25:41 +0800

    🌈 style:
    优化

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import {join, resolve} from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  
   server:{
     port : 8899,
     host:'localhost',
@@ -11,7 +13,7 @@ export default defineConfig({
     }
   },
 
-  
+
   // base: './',
   plugins: [vue()],
   resolve:{
