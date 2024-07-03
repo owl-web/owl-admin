4d6104daffe8f59587b8c19357cb9e279212f991
circleee <upupwangxianxi@163.com>
2024-07-03 21:00:18 +0800

    feat:update

--- src/views/system/components/userRole.vue
+++ src/views/system/components/userRole.vue
@@ -1,37 +1,34 @@
 <template>
-		<div>
-			{{props.id}}
-			<el-table :data="tableData" style="width: 100%; margin-bottom: 20px" row-key="id" border default-expand-all>
-				<el-table-column prop="roleCode" label="角色名称" sortable />
-				<el-table-column prop="sortOrder" label="编辑">
- 
-					<el-button type="error" size="small">删除</el-button>
-				</el-table-column>	
-			</el-table>
-		</div>
+	<div>
+ 		<el-table :data="tableData" style="width: 100%; margin-bottom: 20px" row-key="id" border default-expand-all>
+			<el-table-column prop="roleCode" label="角色名称" sortable />
+			<el-table-column prop="sortOrder" label="编辑">
+
+				<el-button type="error" size="small">删除</el-button>
+			</el-table-column>
+		</el-table>
+	</div>
 </template>
 
 <script setup lang='ts'>
-import {ref} from 'vue'
-import { getUserRole , defineProps } from '@/api/system.js'
- 
+import { ref } from 'vue'
+import { getUserRole, defineProps } from '@/api/system.js'
+
 const props = defineProps({
-  // 基本类型
-  id: String,
- 
+	// 基本类型
+	id: String,
+
 });
 
 const tableData = ref<any>([])
 
-getUserRole({id: props.id}).then(res => {
-  tableData.value = res.data
-  console.log(res)
+getUserRole({ id: props.id }).then(res => {
+	tableData.value = res.data
+	console.log(res)
 })
 
 
 
 </script>
 
-<style lang='less' scoped>
-
-</style>
+<style lang='less' scoped></style>
